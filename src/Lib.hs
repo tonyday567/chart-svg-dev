840ddd7be46d1895424bf8d5a6bbc5e7601f5fd5
Tony Day <tonyday567@gmail.com>
2023-09-18 16:28:10 +1000

    recallibrated text size and scaling

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,32 @@
 {-# OPTIONS_GHC -Wno-unused-imports #-}
+{-# LANGUAGE OverloadedLabels #-}
 
 module Lib
-  (
-  )
+  ( exampleTextChart
+  , exampleText
+  , exampleTextChart')
 where
 
 import Prelude
+import Chart
+import Faker.Lorem
+import Data.Text qualified as T
+import Data.Text (Text)
+import Optics.Core
+import Control.Monad
+
+exampleTextChart :: Int -> Int -> IO [Chart]
+exampleTextChart r c = do
+  ts <- fmap T.pack <$> replicateM r (unwords <$> replicateM c word)
+  let s = defaultTextStyle & #anchor .~ AnchorStart
+  pure $ zipWith (\t x -> TextChart s [(t, Point 0 x)]) ts [0..]
+
+exampleText :: Int -> Int -> IO [Text]
+exampleText r c =
+  fmap T.pack <$> replicateM r (unwords <$> replicateM c word)
+
+exampleTextChart' :: Int -> Int -> TextStyle -> IO [Chart]
+exampleTextChart' r c s = do
+  ts <- fmap T.pack <$> replicateM r (unwords <$> replicateM c word)
+  let s' = s & #anchor .~ AnchorStart
+  pure $ zipWith (\t x -> TextChart s' [(t, Point 0 x)]) ts [0..]
